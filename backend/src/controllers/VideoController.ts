9a92e4a2a8fd380382a8240769ca173dac497690
Somnath Sardar <somnath.sardar.official@gmail.com>
2024-08-07 14:25:56 +0530

    Updated response message

--- backend/src/controllers/VideoController.ts
+++ backend/src/controllers/VideoController.ts
@@ -3,7 +3,6 @@ import path from "path";
 import fs from "fs"
 import { log } from "../utils/log";
 import { v4 as uuidv4 } from 'uuid';
-import { mergeChunks } from "../utils/video";
 import { MergeVideoChunkQueue } from "../queue/producers/video";
 
 /**
@@ -52,9 +51,14 @@ export const Upload = async (req: Request, res: ResponseToolkit) => {
         await fs.promises.writeFile(chunkFilePath, chunk);
 
         // TODO - We need to save this payload into DB and pass the job id to to the queue
-        if (chunkNumber === totalChunks) await MergeVideoChunkQueue({videoPath, totalChunks, originalname});
+        if (chunkNumber === totalChunks) {
+            await MergeVideoChunkQueue({videoPath, totalChunks, originalname})
+            return res.response('Video is uploaded successfully! It will be under review process and you will be notified once it is done.').code(200)
+        }
         
-        return res.response('Video is uploaded successfully! It will be under review process and you will be notified once it is done.').code(200)
+        return res.response({
+            message: `Successfully uploaded chunk ${chunkNumber} out of ${totalChunks} chunks`
+        }).code(200)
     } catch (err: any) {
         log(err.message)
         return res.response('Internal server error!').code(500)
