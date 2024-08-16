e228dd500bf5194b6da8ced820de7c02d08659a9
Somnath Sardar <somnath.sardar.official@gmail.com>
2024-08-16 16:33:00 +0530

    Auth register API created

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -8,3 +8,13 @@ export const login = async (req: Request, res: ResponseToolkit) => {
         return res.response('Internal server error!').code(500)
     }
 }
+
+export const register = async (req: Request, res: ResponseToolkit) => {
+    try {
+        const { name, email, password } = req.payload as { name: string, email: string, password: string }
+        return res.response('Success').code(200)
+    } catch (err: any) {
+        console.log(err.message)
+        return res.response('Internal server error!').code(500)
+    }
+}
--- backend/src/routes/AuthRoutes.ts
+++ backend/src/routes/AuthRoutes.ts
@@ -1,10 +1,26 @@
 import { ServerRoute } from "@hapi/hapi";
 import Joi from "joi";
 import { login } from "../controllers/AuthController";
+import { register } from "module";
 
 const TAGS = ['api', 'Auth']
 
 export const AuthRoutes: ServerRoute[] = [
+    {
+        method: 'POST',
+        path: '/auth/register',
+        options: {
+            tags: TAGS,
+            handler: register,
+            validate: {
+                payload: Joi.object({
+                    name: Joi.string().required(),
+                    email: Joi.string().required().email(),
+                    password: Joi.string().required().min(8),
+                })
+            },
+        },
+    },
     {
         method: 'POST',
         path: '/auth/login',
