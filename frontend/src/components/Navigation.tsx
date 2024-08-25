2501bf1d55cf596cf22ecd92e3bf27e7fae9627b
Somnath Sardar <somnath.sardar.official@gmail.com>
2024-08-25 17:41:14 +0530

    Updated nav menu

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,20 +1,23 @@
 import { NavLink } from "react-router-dom"
+import { useAuth } from "../hooks/useAuth"
 
 export const Navigation = () => {
+    const { isLoggedIn } = useAuth()
     return (
         <ul>
-            <li>
-                <NavLink to='/'>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to='/profile'>Profile</NavLink>
-            </li>
-            <li>
-                <NavLink to='/login'>Sign In</NavLink>
-            </li>
-            <li>
-                <NavLink to='/register'>Sign Up</NavLink>
-            </li>
+            <li><NavLink to='/'>Home</NavLink></li>
+            {
+                isLoggedIn
+                ?   <>
+                        <li><NavLink to='/profile'>Profile</NavLink></li>
+                        <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+                        <li>Logout</li>
+                    </>
+                :   <>
+                        <li><NavLink to='/login'>Sign In</NavLink></li>
+                        <li><NavLink to='/register'>Sign Up</NavLink></li>
+                    </>
+            }
         </ul>
     )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <AuthProvider>
-    <BrowserRouter>
+  <BrowserRouter>
+    <AuthProvider>
       <App />
-    </BrowserRouter>
-  </AuthProvider>
+    </AuthProvider>
+  </BrowserRouter>
 )
